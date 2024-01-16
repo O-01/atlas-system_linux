6e29e66abb5686c60e9f1c73a51d5c89a610e29e
O-01 <sa.an-s@yandex.com>
2024-01-16 05:18:58 -0600

    Saving progress on _getline

--- getline/_getline.c
+++ getline/_getline.c
@@ -1,10 +1,148 @@
 #include "_getline.h"
 
 /**
- * _getline - -
- * @fd: -
- * Return: -
+ * _getline - reads from file descriptor, line-by-line per call until end
+ * @fd: input file descriptor, next line of content requested
+ * Return: next line of file stream as null-terminated string upon success,
+ *         NULL otherwise
 */
 
 char *_getline(const int fd)
-{}
+{
+	static _file_inv *inventory;
+	_file_inv *file = NULL;
+	char *line = NULL;
+
+	if (fd == -1)
+		return (NULL); /* for now, part of task 2 */
+	file = summon(&inventory, fd);
+	if (!file)
+		return (NULL);
+	line = next_line(file);
+	if (!line)
+		free(file->buffer), file->buffer = NULL, free(file);
+	return (line);
+}
+
+/**
+ * summon - queries file list & returns node matching file descriptor
+ * @inv: pointer to first node of file list, if applicable
+ * @fd: input file descriptor by which inv is queried
+ * Return: pointer to file in inventory referenced by fd
+*/
+
+_file_inv *summon(_file_inv **inv, int fd)
+{
+	_file_inv *tmp = *inv;
+	size_t found = 0;
+
+	for (; tmp; tmp = tmp->next)
+		if (tmp->fd == fd)
+		{
+			found = 1;
+			return (tmp);
+		}
+	if (!found)
+		tmp = add_file(inv, fd);
+	return (tmp);
+}
+
+/**
+ * add_file - add file content & rel attributes as node to inv linked list
+ * @inv: pointer to first node of file list, if applicable
+ * @fd: input file descriptor supplied as identifier for added node
+ * Return: pointer to added node upon success, NULL otherwise
+*/
+
+_file_inv *add_file(_file_inv **inv, int fd)
+{
+	_file_inv *add = NULL, *file = *inv;
+	char *file_content = NULL;
+
+	add = malloc(sizeof(_file_inv));
+	if (!add)
+		return (NULL);
+	add->fd = fd;
+	add->marker = 0;
+	add->next = NULL;
+	file_content = malloc(READ_SIZE);
+	if (!file_content)
+		return (NULL);
+	add->buffer_size = read(fd, file_content, READ_SIZE);
+	add->buffer = _realloc(file_content, add->buffer_size);
+	if (!add->buffer)
+		return (NULL);
+	if (!(*inv))
+		*inv = add;
+	else
+		for (; file; file = file->next)
+			if (!file->next)
+				file->next = add;
+	return (add);
+}
+
+/**
+ * next_line - pull next line from buffer of node related to input file
+ * @file: input file contents from which to extract next line
+ * Return: next line from buffer of input file stream as null-terminated string
+ *         upon success, NULL otherwise
+*/
+
+char *next_line(_file_inv *file)
+{
+	size_t mark = file->marker, span = 0, end = file->buffer_size;
+	char *txt = file->buffer, *line = NULL;
+
+	if (mark < end)
+	{
+		for (
+			;
+			txt[mark] != '\n' &&
+			txt[mark] != '\0' &&
+			mark != end;
+			mark++, span++
+		)
+			;
+		line = malloc(sizeof(char) * span + 1);
+		if (!line)
+			return (NULL);
+		for (span = 0; file->marker != mark; file->marker++, span++)
+			line[span] = txt[file->marker];
+		line[span] = '\0';
+		if (txt[mark] == '\0')
+			free(txt), txt = NULL;
+		file->marker++;
+	}
+	return (line);
+}
+
+/**
+ * _realloc - reallocate memory segment
+ * @input: allocated memory segment to be adjusted
+ * @desired_size: size to which input to be reallocated
+ * Return: pointer to reallocated memory upon success
+*/
+
+void *_realloc(void *input, ssize_t desired_size)
+{
+	void *update = NULL;
+
+	if (input && !desired_size)
+	{
+		free(input);
+		return (NULL);
+	}
+	else
+	{
+		update = malloc(desired_size);
+		if (!update)
+		{
+			if (input)
+				free(input);
+			return (NULL);
+		}
+		memcpy(update, input, desired_size);
+		free(input);
+		return (update);
+	}
+}
--- getline/_getline.h
+++ getline/_getline.h
@@ -1,8 +1,38 @@
 #ifndef _GETLINE_H_
 #define _GETLINE_H_
 
-#define READ_SIZE
+#define READ_SIZE (1024 * 1024 * 4)
+#define STOP ('\n' && '\0')
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * struct _file_inventory - node template designed to handle contents related
+ *                          to an input file descriptor as linked list
+ * @fd: file descriptor as identifier
+ * @buffer: buffer to contain applicable text content from file stream
+ * @buffer_size: bytes read from file stream
+ * @marker: keeps track of cursor position within buffer
+ * @next: pointer to next node if present, else NULL
+ */
+
+typedef struct _file_inventory
+{
+	int fd;
+	char *buffer;
+	ssize_t buffer_size;
+	size_t marker;
+	struct _file_inventory *next;
+} _file_inv;
 
 char *_getline(const int fd);
+_file_inv *summon(_file_inv **inv, int id);
+_file_inv *add_file(_file_inv **inv, int fd);
+char *next_line(_file_inv *file);
+void *_realloc(void *mem, ssize_t desired_size);
 
 #endif
