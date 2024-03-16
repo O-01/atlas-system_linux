7d400e2a514a7815202a87ae98a2f34b5bd5fd04
O-01 <sa.an-s@yandex.com>
2024-03-15 23:25:03 -0500

    Saving progress on 2-handle_sigaction (signals)

--- signals/signals.h
+++ signals/signals.h
@@ -1,13 +1,19 @@
 #ifndef _SIGNALS_H_
 #define _SIGNALS_H_
 
+#define _XOPEN_SOURCE 700
+
+#include <features.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef struct sigaction sigact_t;
+
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
 
 #endif
