1d2137702755cd05ad0f4a7030633ed290f3c654
O-01 <sa.an-s@yandex.com>
2024-05-01 15:04:15 -0500

    Added 1-tprintf (multithreading)

---
+++ multithreading/1-tprintf.c
@@ -0,0 +1,11 @@
+#include "multithreading.h"
+
+/**
+ * tprintf - uses printf family to print input formatted string
+ * @format: format string to be printed
+ * Return: number of characters successfully printed
+*/
+int tprintf(char const *format, ...)
+{
+	return (printf("[%lu] %s", pthread_self(), format));
+}
